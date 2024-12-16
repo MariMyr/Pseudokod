f4f66380352027e3066becdcbf07000e1acdbb02
Mari Myrmoen <mari.myr@gmail.com>
2024-12-16 15:20:55 +0100

    Added pesudocode for G-version Split the nota

---
+++ script.js
@@ -0,0 +1,45 @@
+// G-Version - Split the Nota
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+/*
+SET variabel sum
+SET variabel persons
+SET variabel tip
+
+WHILE true DO
+    SET variabel tip
+    IF tip >= 0 AND tip <= 1 THEN
+       BREAK
+    ELSE
+       PRINT “Dricks måste anges som ett decmialtal mellan 0 och 1, (t.ex. 0.10 för 10%)”
+    END IF
+END WHILE
+
+totalSum= sum + (sum * tip)
+sumPerPerson = totalSum / persons
+PRINT "Varje person ska betala:" + sumPerPerson
+*/
+
+// VG-Version - Lewis Carroll Word Puzzle
+// Skriv din pseudokod innanför nedanstående kommentarsblock
+// Startkoden är 100% frivillig och kan tas bort eller skrivas om
+/*
+
+play();
+
+function play()
+    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startOrd till "FOUR";
+    SET variabel slutordOrd till "FIVE";
+
+
+end function
+
+function isOneLetterApart(wordOne, wordTwo)
+    SET variabel diffCount till 0;
+
+    // Här behöver du skriva koden för din funktion
+
+    return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
+end function
+
+*/
