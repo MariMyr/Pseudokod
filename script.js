b2993cf15acb1c287c69588a0c1da54d9042d680
Mari Myrmoen <mari.myr@gmail.com>
2024-12-17 15:17:31 +0100

    Added pseudocode for VG

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ WHILE true DO
     END IF
 END WHILE
 
-totalSum= sum + (sum * tip)
+totalSum = sum + (sum * tip)
 sumPerPerson = totalSum / persons
 PRINT "Varje person ska betala:" + sumPerPerson
 */
@@ -27,19 +27,46 @@ PRINT "Varje person ska betala:" + sumPerPerson
 play();
 
 function play()
-    SET variabel ordbok = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
-    SET variabel startOrd till "FOUR";
-    SET variabel slutordOrd till "FIVE";
+    SET variabel dictionary = [FOUR, FOUL, FOOL, FOOT, FORT, FORE, FIRE, FIVE]; // Innehåller ALLA ord i det engelska språket
+    SET variabel startWord till "FOUR";
+    SET variabel endWord till "FIVE";
+    SET variabel guessCount till 0
 
+    WHILE startWord != endWord DO
+        INPUT userInput
+        guessCount++
 
-end function
+        IF userInput NOT IN dictionary
+            PRINT “Ordet finns inte i ordboken. Forsök igen. Du har gissat” + guessCount “gånger.”
+        ELSE IF NOT isOneLetterApart(startWord, userInput)
+            PRINT “Ordet skljer sig med mer än en bokstav. Forsök igen. Du har gissat” + guessCount “gånger”.
+        ELSE
+            SET startWord = userInput
+            IF userInput === endWord
+                PRINT “Grattis! Du vann på” + guessCount “gissningar!”
+            ELSE
+                PRINT “Du har gissat rätt! Du har gissat” + guessCount “gånger”. 
+            END IF
+        END IF
+    END WHILE
+END FUNCTION        
 
-function isOneLetterApart(wordOne, wordTwo)
-    SET variabel diffCount till 0;
+FUNCTION isOneLetterApart(startWord, userInput)
+    IF LENGTH (startWord) != LENGTH (userInput)
+        RETURN FALSE
+    END IF
 
-    // Här behöver du skriva koden för din funktion
+    SET variabel diffCount till 0
+    FOR varje tecken, index 'i', from 0 TO length (startWord) -1
+        IF tecken i startWord[i] != tecken i userInput[i]
+            diffCount++
+            IF diffCount > 1
+                RETURN FALSE
+            END IF
+        END IF
+    END FOR
 
     return diffCount === 1; // returnerar sant om endast en bokstav ändrats, annars falskt
-end function
+END FUNCTION
 
 */
